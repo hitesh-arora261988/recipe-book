877c881ae3be64017d9fa1d868523eaa88c7c5a2
Hitesh <hitesh.arora261988@gmail.com>
2019-02-03 22:32:12 -0500

    [Reciepe Book]

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { HomeComponent } from './core/home/home.component';
+
+const appRoutes: Routes = [
+  /*{ path: '', redirectTo: '/recipes', pathMatch: 'full' },*/
+  { path: '', component: HomeComponent },
+  {path: 'recipes',  loadChildren: './recipes/recipes.module#RecipeModule'},
+  { path: 'shopping-list', component: ShoppingListComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
